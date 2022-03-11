5f57dc17a935f4be4d7d2059b5d0c8fffa3ec0ed
= <=>
2022-03-11 19:41:33 +0000

    complete order challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.scss";
+import music from "./images/icon-music.svg";
+import illustration from "./images/illustration-hero.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="card-img">
+        <img src={illustration} alt="" />
+      </div>
+      <div className="card-container">
+        <div className="card-info">
+          <h1>Order Summary</h1>
+          <p>
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like!
+          </p>
+        </div>
+        <div className="plan">
+          <div className="left">
+            <img src={music} alt="music" />
+            <div>
+              <p id="plan-title">Annual Plan</p>
+              <p>$59.99/year</p>
+            </div>
+          </div>
+          <button id="change">Change</button>
+        </div>
+        <button id="payment-btn">Proceed to Payment</button>
+        <button id="cancel">Cancel order</button>
+      </div>
     </div>
   );
 }
